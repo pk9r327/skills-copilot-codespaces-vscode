f8a2d3299d8dd2bfb4fdf6b36b675ca0bb140284
La Minh Phuc <72692627+pk9r327@users.noreply.github.com>
2023-12-19 10:19:45 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,31 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Store comments
+const commentsByPostId = {};
+
+// Get comments by post id
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+// Create comment
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments);
+});
+
+// Listen on port 4001
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
